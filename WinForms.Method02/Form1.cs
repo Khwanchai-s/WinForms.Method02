3471cca28d85a0b475587894dc2021c9b8254b55
Khwanchai-s <khwanchai.s@gmail.com>
2024-12-11 09:21:38 +0700

    update1

--- WinForms.Method02/Form1.cs
+++ WinForms.Method02/Form1.cs
@@ -9,19 +9,45 @@ namespace WinForms.Method02
 
         private void btnCircleArea_Click(object sender, EventArgs e)
         {
-            double r = 0;
-            if (!double.TryParse(txtRadius.Text, out r))
-            {
-                MessageBox.Show("ข้อมูลไม่ถูกต้อง", "เกิดข้อผิดพลาด");
-                return;
-            }
-            CircleArea(r);   //เรียกใช้ Void Method 
+            //double r = 0;
+            //if (!double.TryParse(txtRadius.Text, out r))
+            //{
+            //    MessageBox.Show("ข้อมูลไม่ถูกต้อง", "เกิดข้อผิดพลาด");
+            //    return;
+            //}
+            //CircleArea(r);   //เรียกใช้ Void Method ส่งค่า double
+
+            //CircleArea(txtRadius);   //เรียกใช้ viod method แบบส่ง textbox
+            CircleArea(txtRadius, lblResult);
         }
 
         void CircleArea(double radius)
         {
             double area = Math.PI * radius * radius;
             lblResult.Text = area.ToString();
+        }
+
+        void CircleArea(TextBox txt)
+        {
+            double r = 0;
+            if (!double.TryParse(txt.Text, out r))
+            {
+                MessageBox.Show("ข้อมูลไม่ถูกต้อง", "เกิดข้อผิดพลาด");
+                return;
+            }
+            double area = Math.PI * r * r;
+            lblResult.Text = area.ToString();
+        }
+        void CircleArea(TextBox txt, Label lbl)
+        {
+            double r = 0;
+            if (!double.TryParse(txt.Text, out r))
+            {
+                MessageBox.Show("ข้อมูลไม่ถูกต้อง", "เกิดข้อผิดพลาด");
+                return;
+            }
+            double area = Math.PI * r * r;
+            lbl.Text = area.ToString();
         }
     }
 }
